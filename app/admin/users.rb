c5ede3e5414cbc98dae20c56f8f2543d85fb7c02
Vyacheslav <truesspay@gmail.com>
2020-10-28 13:06:22 +0200

    add AdminUser, restyling AA pages

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,5 +1,8 @@
 ActiveAdmin.register User do
 
+  filter :email
+  filter :first_name
+  filter :last_name
 
   show do
     attributes_table do
@@ -12,5 +15,22 @@ ActiveAdmin.register User do
       row :created_at
       row :updated_at
     end
+  end
+
+  form title: 'A custom title' do |f|
+    inputs 'Info' do
+      input :email
+      input :first_name, label: "First name"
+      input :last_name, label: "Last name"
+      datetime_select :created_at, label: "Created at"
+    end
+  end
+
+  index do
+    selectable_column
+    column :email
+    column :first_name
+    column :last_name
+    actions
   end
 end
