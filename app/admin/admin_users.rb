95b071ae9182ba24bcbe82287e760976aca993f6
SsPay <sapsay.99@mail.ru>
2020-10-29 16:20:00 +0200

    Destroy User model

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -1,28 +1,45 @@
 ActiveAdmin.register AdminUser do
-  permit_params :email, :password, :password_confirmation
+  permit_params :email, :password, :password_confirmation, :first_name, :last_name
+
+  filter :email
+  filter :first_name
+  filter :last_name
+  filter :created_at
+
+  config.sort_order = 'last_name_asc'
 
   index do
     selectable_column
-    id_column
     column :email
-    column :current_sign_in_at
-    column :sign_in_count
-    column :created_at
+    column :first_name
+    column :last_name
     actions
   end
 
-  filter :email
-  filter :current_sign_in_at
-  filter :sign_in_count
-  filter :created_at
-
-  form do |f|
-    f.inputs do
-      f.input :email
-      f.input :password
-      f.input :password_confirmation
+  show do
+    attributes_table do
+      row :email
+      row :full_name do
+        "#{admin_user.first_name} #{admin_user.last_name}"
+      end
     end
-    f.actions
+  end
+
+  form title: 'A custom title' do |f|
+    inputs 'Info' do
+      input :email
+      input :first_name, label: "First name"
+      input :last_name, label: "Last name"
+      input :password, label: "Password"
+      actions
+    end
+  end
+
+  sidebar :Details, only: :show do
+    attributes_table do
+      row :created_at
+      row :updated_at
+    end
   end
 
 end
