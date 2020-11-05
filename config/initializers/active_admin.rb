43223bb9ff67b1c69050990b54993aa480b3fff3
Igor <izawgorodskiy@ergoserv.com>
2020-11-05 13:55:02 +0200

    Added activeadmin and validations for first and last names

---
+++ config/initializers/active_admin.rb
@@ -0,0 +1,11 @@
+ActiveAdmin.setup do |config|
+  config.site_title = "Ems"
+  config.authentication_method = :authenticate_admin_user!
+  config.current_user_method = :current_admin_user
+  config.logout_link_path = :destroy_admin_user_session_path
+  config.comments = false
+  config.comments_menu = false
+  config.batch_actions = true
+  config.filter_attributes = [:encrypted_password, :password, :password_confirmation]
+  config.localize_format = :long
+end
