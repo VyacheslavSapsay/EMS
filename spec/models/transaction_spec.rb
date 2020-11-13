79cdf556c9edc6d3216472aa9656c92a55fb725b
SsPay <sapsay.99@mail.ru>
2020-11-13 19:31:37 +0200

    add some tests

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -16,14 +16,29 @@ RSpec.describe Transaction, type: :model do
   end
 
   it 'validates that transaction with date' do
-    transaction = Transaction.new(description: 'desc', debit_account_id: 1, debit_amount: 5)
+    transaction = described_class.new
     expect(transaction).to be_invalid
     expect(transaction.errors[:occured_at]).to include("can't be blank")
   end
 
   it 'validates that *_amount is greater than 0' do
-    transaction = Transaction.new(description: 'desc', debit_account_id: 1, debit_amount: 0)
+    transaction = described_class.new(debit_account_id: 1, debit_amount: 0)
     expect(transaction).to be_invalid
     expect(transaction.errors[:debit_amount]).to include("Can't be blank")
   end
+
+  it 'validates that *_account is selected' do
+    transaction = described_class.new(description: 'description',
+      occured_at: DateTime.now, author_id: 1)
+    expect(transaction.valid?).to be_falsey
+    expect(transaction.errors[:base].size).to eq(1)
+    expect(transaction.errors[:base]).to include("Choose debit or credit account")
+  end
+
+  it 'author must exist' do
+    transaction = described_class.new(author_id: '')
+    expect(transaction).to be_invalid
+    expect(transaction.errors[:author]).to include("must exist")
+  end
+
 end
