f87ef6361022b040ba5e22f8fe0974c9fef3aa8f
Vyacheslav <sapsay.99@mail.ru>
2020-11-17 11:28:46 +0200

    fix bug on deleting account

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,5 +1,12 @@
 class Account < ApplicationRecord
-  include Transactionable
+  has_many :debit_account_transactions,
+  class_name: 'Transaction',
+  foreign_key: 'debit_account_id',
+  dependent: :nullify
+has_many :credit_account_transactions,
+  class_name: 'Transaction',
+  foreign_key: 'credit_account_id',
+  dependent: :nullify
 
   def transactions
     Transaction.where("credit_account_id = ? OR debit_account_id = ?", id, id)
